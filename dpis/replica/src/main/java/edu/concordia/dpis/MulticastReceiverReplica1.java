b6be29b860d76f29f4dd33482ecf4e0ae364eb7b
Aliasgarmala <aliasgar.mala@gmail.com>
2013-12-02 19:49:21 -0500

    Aliasgar commit

---
+++ dpis/replica/src/main/java/edu/concordia/dpis/MulticastReceiverReplica1.java
@@ -0,0 +1,11 @@
+package edu.concordia.dpis;
+
+public class MulticastReceiverReplica1 {
+
+	public static void main(String[] args) {
+		//anyone replica who wants to be the replica other than leader will have to invoke this thread on port 4446
+		(new Thread(new MulticastReceiver(4446))).start();
+
+	}
+
+}
---
+++ dpis/replica/src/main/java/edu/concordia/dpis/MulticastServerMain.java
@@ -0,0 +1,11 @@
+package edu.concordia.dpis;
+
+public class MulticastServerMain {
+
+	public static void main(String[] args) {
+		//anyone replica who wants to be the leader will have to invoke this thread on port 4446
+		new Thread(new MulticastServer(4446)).start();  
+
+	}
+
+}
